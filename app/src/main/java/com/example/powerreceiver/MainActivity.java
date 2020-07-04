79814f003ef8abc62c0e1fe6b577c5d9624f7208
StepehnLeslie-Coder <stephenleslie942@gmail.com>
2020-07-04 13:25:35 -0500

    initial commit

---
+++ app/src/main/java/com/example/powerreceiver/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.powerreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Customreceiver mReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mReceiver= new Customreceiver();
+        IntentFilter filter= new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        this.registerReceiver(mReceiver,filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        this.unregisterReceiver(mReceiver);
+        super.onDestroy();
+
+    }
+}
